191caf6c30e15c3ab3f1cd2bbf5a01df69961323
Antony Lewis <antony@cosmologist.info>
2019-11-18 12:26:05 +0000

    fix install

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name="plancklens",
       description='Example external Cobaya likelihood package: Planck 2018 lensing',
       zip_safe=False,  # set to false if you want to easily access bundled package data files
       packages=['plancklens', 'plancklens.tests'],
-      package_data={'plancklens': ['data_2018/*', 'data_2018/**/*']},
+      package_data={'plancklens': ['*.yaml', '*.bibtex', 'data_2018/*', 'data_2018/**/*']},
       install_requires=['cobaya>=2.0.5'],
       test_suite='plancklens.tests',
       tests_require=['camb>=1.0.5']
