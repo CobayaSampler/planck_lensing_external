072b136cddf7d6327b11b18e72dc7d676d827f1f
torradocacho <JesusTorrado@users.noreply.github.com>
2021-01-31 13:04:12 +0100

    leading underscore removed from cmblikes

--- plancklensing/plancklensing.py
+++ plancklensing/plancklensing.py
@@ -1,7 +1,7 @@
-from cobaya.likelihoods._base_classes import _CMBlikes
+from cobaya.likelihoods._base_classes import CMBlikes
 
 
-class PlanckLensing(_CMBlikes):
+class PlanckLensing(CMBlikes):
     # You can either keep data separate and set install_options for where to download from, or
     # (if not too big) bundle the data in the package, so don't need separate install as here
     install_options = {}
